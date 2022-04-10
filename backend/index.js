17f0510c9bcb4bafdf7b26a1adb589b3024834a5
Bansi <jpatel19762001@gmail.com>
2022-04-10 10:49:56 +0530

    basic bugs fixed

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ app.use("/users", require("./routes/users"));
 app.use(require('./routes/payment'));
 
 // deployment
-if (process.env.NODE_ENV === "production")
+if (process.env.NODE_ENV == "PRODUCTION")
 {
     
     app.use(express.static(path.join(__dirname, "../frontend/build")));
